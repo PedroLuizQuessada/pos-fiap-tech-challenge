8b56a685edeb16ee0c65e0c2b6f1f1bb60345353
Pedro Quessada <pedroluiz.quessada@gmail.com>
2025-04-11 09:31:42 -0300

    feat: exclusão de usuário

--- src/main/java/com/example/tech_challenge/repo/UserRepository.java
+++ src/main/java/com/example/tech_challenge/repo/UserRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.Date;
 
@@ -21,10 +22,15 @@ public interface UserRepository extends JpaRepository<User, Long> {
     @Query("SELECT COUNT(*) FROM User u WHERE u.login = :login")
     Integer countUserByLoginEquals(@Param("login") String login);
 
-    @org.springframework.transaction.annotation.Transactional
+    @Transactional
     @Modifying
     @Query("UPDATE User u SET u.name = :name, u.email = :email, u.login = :login, u.address = :address, " +
             "u.lastUpdateDate = :lastUpdateDate WHERE u.login = :oldLogin")
     void updateByLogin(@Param("name") String name, @Param("email") String email, @Param("login") String login,
                        @Param("address") String address, @Param("lastUpdateDate") Date lastUpdateDate, @Param("oldLogin") String oldLogin);
+
+    @Transactional
+    @Modifying
+    @Query("DELETE User u WHERE u.login = :login")
+    void deleteByLogin(@Param("login") String login);
 }
