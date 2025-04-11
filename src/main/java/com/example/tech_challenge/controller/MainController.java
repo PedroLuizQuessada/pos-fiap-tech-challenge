22c618e335a50f890234993713a5e0eb9785ef54
Pedro Quessada <pedroluiz.quessada@gmail.com>
2025-04-11 01:03:55 -0300

    feat: refatorando handleError

--- src/main/java/com/example/tech_challenge/controller/MainController.java
+++ src/main/java/com/example/tech_challenge/controller/MainController.java
@@ -1,6 +1,8 @@
 package com.example.tech_challenge.controller;
 
+import com.example.tech_challenge.exception.UnauthorizedActionException;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -22,35 +24,46 @@ public class MainController implements ErrorController {
     }
 
     @RequestMapping("/error")
-    public String handleError(HttpServletRequest request, Model model) {
+    public String handleError(HttpServletRequest request, HttpServletResponse response, Model model) {
         String errorMessage = "";
         StringBuilder stackTrace = new StringBuilder();
-        if ((Integer) request.getAttribute("jakarta.servlet.error.status_code") == 404) {
-            errorMessage = "Página não encontrada";
-        }
-        else if ((Integer) request.getAttribute("jakarta.servlet.error.status_code") == 403) {
-            errorMessage = "Página proibida";
-        }
-        else if ((Integer) request.getAttribute("jakarta.servlet.error.status_code") == 401) {
-            errorMessage = "Página nao autorizada";
-        }
-        else if ((Integer) request.getAttribute("jakarta.servlet.error.status_code") == 400) {
-            errorMessage = "Requisiçao incorreta";
-            MethodArgumentNotValidException obj = ((MethodArgumentNotValidException) request.getAttribute("org.springframework.boot.web.servlet.error.DefaultErrorAttributes.ERROR"));
-            if (obj != null) {
-                for (Object e : obj.getDetailMessageArguments()) {
-                    stackTrace.append(e.toString()).append("\n");
+
+        switch ((Integer) request.getAttribute("jakarta.servlet.error.status_code")) {
+            case 404:
+                errorMessage = "Página não encontrada";
+                break;
+
+            case 403:
+                errorMessage = "Página proibida";
+                break;
+
+            case 401:
+                errorMessage = "Página nao autorizada";
+                break;
+
+            case 400:
+                errorMessage = "Requisiçao incorreta";
+                MethodArgumentNotValidException methodArgumentNotValidException = ((MethodArgumentNotValidException) request.getAttribute("org.springframework.boot.web.servlet.error.DefaultErrorAttributes.ERROR"));
+                if (methodArgumentNotValidException != null) {
+                    for (Object e : methodArgumentNotValidException.getDetailMessageArguments()) {
+                        stackTrace.append(e.toString()).append("\n");
+                    }
                 }
-            }
-        }
-        else {
-            Exception obj = (Exception) request.getAttribute("org.springframework.boot.web.servlet.error.DefaultErrorAttributes.ERROR");
-            if (obj != null) {
-                errorMessage = obj.getMessage();
-                for (StackTraceElement e : obj.getStackTrace()) {
-                    stackTrace.append(e.getClassName()).append(".").append(e.getMethodName()).append("(").append(e.getFileName()).append(":").append(e.getLineNumber()).append(")\n");
+                break;
+
+            default:
+                Exception exception = (Exception) request.getAttribute("org.springframework.boot.web.servlet.error.DefaultErrorAttributes.ERROR");
+                if (exception != null) {
+                    errorMessage = exception.getMessage();
+                    for (StackTraceElement e : exception.getStackTrace()) {
+                        stackTrace.append(e.getClassName()).append(".").append(e.getMethodName()).append("(").append(e.getFileName()).append(":").append(e.getLineNumber()).append(")\n");
+                    }
+
+                    if (exception instanceof UnauthorizedActionException) {
+                        response.setStatus(403);
+                    }
                 }
-            }
+                break;
         }
 
         model.addAttribute("errorMessage", errorMessage);
