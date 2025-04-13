9eba957f6f62716618096092ac47d1b04433c0f6
Pedro Quessada <pedroluiz.quessada@gmail.com>
2025-04-13 00:17:49 -0300

    fix: retirando imports desnecessários

--- src/main/java/com/example/tech_challenge/repo/AddressRepository.java
+++ src/main/java/com/example/tech_challenge/repo/AddressRepository.java
@@ -2,17 +2,8 @@ package com.example.tech_challenge.repo;
 
 import com.example.tech_challenge.domain.address.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface AddressRepository extends JpaRepository<Address, Integer> {
-
-//    @Transactional
-//    @Modifying
-//    @Query("DELETE FROM Address a WHERE a.id = :id")
-//    void deleteById(@Param("id") Integer id);
 }
