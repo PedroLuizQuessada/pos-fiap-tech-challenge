639468df0f31d90ec357e6f9094f727f6e240669
Pedro Quessada <pedroluiz.quessada@gmail.com>
2025-07-21 15:06:57 -0300

    fix: adicionando last_update_date no usuário nativo

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,4 +2,4 @@ INSERT IGNORE INTO addresses (address_state, city, street, address_number, zip_c
 INSERT IGNORE INTO user_types (type_name) VALUES ("ADMIN");
 INSERT IGNORE INTO user_types (type_name) VALUES ("OWNER");
 INSERT IGNORE INTO user_types (type_name) VALUES ("CUSTOMER");
-INSERT IGNORE INTO users (user_name, email, login, password, user_type, address) VALUES ("nome usuario 1", "email1@gmail.com", "LoginUsuario1", "$2a$10$UqmSCvzIURnuZj3a5m/VXu1fma2hWZJVsYYJZVxmsypJZGqVWKcKC", 1, 1);
+INSERT IGNORE INTO users (user_name, email, login, password, user_type, address, last_update_date) VALUES ("nome usuario 1", "email1@gmail.com", "LoginUsuario1", "$2a$10$UqmSCvzIURnuZj3a5m/VXu1fma2hWZJVsYYJZVxmsypJZGqVWKcKC", 1, 1, CURRENT_DATE);
